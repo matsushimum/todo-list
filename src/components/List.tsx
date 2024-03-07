911c0db058bf4cbf24acf97b7b79d3d4c1eecf5c
matsushimum <taiseim25@gmail.com>
2024-03-07 13:37:39 +0900

    [add]Form以外の要素を追加

---
+++ src/components/List.tsx
@@ -0,0 +1,34 @@
+import { Item } from "./Item";
+import { Todo } from "./Todo";
+
+//受け取るPropsの型を定義する
+type Listprops = {
+    todos: Todo[];
+    deleteTodo: Function;
+    updateTodo: Function;
+};
+
+const List:React.FC<Listprops> = ({todos,deleteTodo,updateTodo}) => {
+    //押したTODOのidを[deleteTodo]に渡す
+    const complete = (id:number) => {
+        deleteTodo(id);
+    };
+    return(
+        <div>
+            //TODOリストを取得して[map関数]で1つ1つ取り出す
+            {todos.map((todo) => {
+                return(
+                    //[Item]に必要なPropsを渡す
+                    <Item
+                        Key = {todo.id}
+                        todo = {todo}
+                        complete = {complete}
+                        updateTodo = {updateTodo}
+                     />
+                );
+            })}
+        </div>
+    );
+};
+
+export default List;
